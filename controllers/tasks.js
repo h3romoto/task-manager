740beee593536b00d30aa330a1e957a5600d3fc3
Tanaka Khondowe <t.khondowe@gmail.com>
2022-09-25 15:42:09 -0500

    Fixing error Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client by adding return statement to controller functions

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,37 +4,38 @@
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await  Task.find({})
-    res.status(200).json({tasks})
+    return res.status(200).json({tasks})
   } catch (error) {
-    res.status(500).json({ msg: error})
+    return es.status(500).json({ msg: error})
   }
 
   res.send('All tasks available')
 }
 
+
 const createTask = async (req, res) => {
   // create new Mongo Document, Task
   try {
     const task = await Task.create(req.body)
-    res.status(201).json({ task })
+    return res.status(201).json({ task })
   } catch (error) {
-    res.status(500).json({ msg: error})
+    return res.status(500).json({ msg: error})
   }
 }
 
 
 const getTask = (req, res) => {
-  res.json({id:req.params.id})
+  return res.json({id:req.params.id})
 }
 
 
 const updateTask = (req, res) => {
-  res.send('Update a task')
+  return res.send('Update a task')
 }
 
 
 const deleteTask = (req, res) => {
-  res.send('Delete a task')
+  return res.send('Delete a task')
 }
 
 
