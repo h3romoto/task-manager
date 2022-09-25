af2ffa4842997579844a0196a50a0f39183acbf5
Tanaka <t.khondowe@gmail.com>
2022-09-25 16:29:38 -0500

    Delete a task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -6,10 +6,8 @@ const getAllTasks = async (req, res) => {
     const tasks = await  Task.find({})
     return res.status(200).json({tasks})
   } catch (error) {
-    return es.status(500).json({ msg: error})
+    return res.status(500).json({ msg: error})
   }
-
-  res.send('All tasks available')
 }
 
 
@@ -39,13 +37,24 @@ const getTask = async (req, res) => {
 }
 
 
-const updateTask = (req, res) => {
-  return res.send('Update a task')
+const deleteTask = async (req, res) => {
+  const {id: taskID} = req.params
+  try {
+    const task = await Task.findOneAndDelete({ _id: taskID})
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with ID: ${taskID}` })
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+
+  return res.send(`Deleted task ${taskID}`)
 }
 
 
-const deleteTask = (req, res) => {
-  return res.send('Delete a task')
+const updateTask = (req, res) => {
+  return res.send('Update a task')
 }
 
 
