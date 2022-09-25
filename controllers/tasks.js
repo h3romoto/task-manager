4f82df3fc0cd4e2a63cfce0aff1d75acee7e14c1
Tanaka Khondowe <t.khondowe@gmail.com>
2022-09-25 15:31:47 -0500

    Get all tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,27 +1,42 @@
  const Task = require('../models/tasks')
 
-// callback functions
-const getAllTasks = (req, res) => {
+// CRUD callback functions
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await  Task.find({})
+    res.status(200).json({tasks})
+  } catch (error) {
+    res.status(500).json({ msg: error})
+  }
+
   res.send('All tasks available')
 }
 
 const createTask = async (req, res) => {
   // create new Mongo Document, Task
-  const task = await Task.create(req.body)
-  res.status(200).json({ task })
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error})
+  }
 }
+
 
 const getTask = (req, res) => {
   res.json({id:req.params.id})
 }
 
+
 const updateTask = (req, res) => {
   res.send('Update a task')
 }
 
+
 const deleteTask = (req, res) => {
   res.send('Delete a task')
 }
+
 
 module.exports = {
   getAllTasks,
