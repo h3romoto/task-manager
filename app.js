ec1e47dfb4082ff5dc0322e4646c1a1489061799
Tanaka <t.khondowe@gmail.com>
2022-09-25 23:37:33 -0500

    Custom error handler middleware

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 require('./db/connect')
 const express = require('express')
 const app = express();
-const port = 3000
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 require('dotenv').config()
 
-
-app.get('/hello', (req, res) => {
-  res.send('Task Manager App')
-})
 
 // middleware
 app.use(express.static('./public'))
 app.use(express.json())
+app.use(errorHandlerMiddleware)
 // how does this work?
-// app.use(notFound) 
+app.use(notFound) 
+
+const port = 3000
 
 // root route for tasks
 app.use('/api/v1/tasks', tasks)
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,7 @@
  const Task = require('../models/tasks')
  const asyncWrapper = require('../middleware/async')
-
+const { createCustomError } = require('../errors/custom-errors')
+const { create } = require('../models/tasks')
 
 // CRUD callback functions
 const getAllTasks = asyncWrapper ( async (req, res) => {
@@ -21,7 +22,7 @@ const getTask = asyncWrapper ( async (req, res) => {
     const {id: taskID} = req.params
     const task = await Task.findOne({ _id: taskID})
     if (!task) {
-      return res.status(404).json({ msg: `No message with id : ${taskID}` })
+      return next(createCustomError(`No message with id : ${taskID}`, 404))
     }
     res.json({ task })
 })
@@ -31,9 +32,7 @@ const deleteTask = asyncWrapper ( async (req, res) => {
   const {id: taskID} = req.params
     const task = await Task.findOneAndDelete({ _id: taskID})
     if (!task) {
-      return res
-                .status(404)
-                .json({ msg: `No task with ID: ${taskID}` })
+      return next(createCustomError(`No message with id : ${taskID}`, 404))
     }
   return res.send(`Deleted task ${taskID}`)
 })
@@ -50,9 +49,7 @@ const updateTask = asyncWrapper ( async (req, res) => {
               runValidators: true
           })
   if (!task) {
-    res
-    .status(404)
-    .json({ msg: `Task ${taskID} does not exist` })
+    return next(createCustomError(`No message with id : ${taskID}`, 404))
   }
   return res.status(200).json({ task })
 })
